f7194a9e1dabdf1b0c29a8588a55ddb4bb8f989f
Anuj Ambadas Giri <87684592+anujgiri20@users.noreply.github.com>
2022-06-25 09:59:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -127,19 +127,19 @@ app.post("/login" , async(request,response)=>{
 
 
 //opeartions on data base
-app.post("/insertToKepper", async(request,response)=>{
+app.post("/bank", async(request,response)=>{
     const client = await createconnections()
     const add_data = request.body
     const result = await client.db("kepper").collection("bank").insertMany(add_data)
     response.send(result)
 })
-app.get("/getFromKepper", async(request,response)=>{
+app.get("/bank", async(request,response)=>{
     const client = await createconnections()
     const result = await client.db("kepper").collection("bank").find({}).toArray()
     response.send(result)
 
 })
-app.delete("/deleteFromKepper/:id" , async(request,response)=>{
+app.delete("/bank/:id" , async(request,response)=>{
 const id = request.params.id
 const client = await createconnections()
 const user= await client.db("kepper").collection("bank").deleteOne({_id: new mongodb.ObjectId(id)})
@@ -151,7 +151,7 @@ const user= await client.db("kepper").collection("bank").deleteOne({_id: new mon
 })
 
 
-app.put("/patchkepper/:id", async(request, response) => {
+app.put("/bank/:id", async(request, response) => {
     const id = request.params.id;
     const client = await createconnections();
     const user= await client.db("kepper").collection("bank").updateOne({_id:new mongodb.ObjectId(id)},{$set:{"name":request.body.name,"money":request.body.money}})
